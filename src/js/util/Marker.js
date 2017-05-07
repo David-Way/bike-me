9ef28109dd1b8646dca6012c2c342b92d90cf457
David Way <dway@ie.ibm.com>
2017-05-07 08:19:46 +0100

    Additional functionality added to marker object
    - generate static station marker
    - add self to map
    - set own lat and long

--- src/js/util/Marker.js
+++ src/js/util/Marker.js
@@ -4,13 +4,22 @@ var L = require('leaflet');
 
 var Marker = function(_markerType, _coOrdinates) {
 	this.markerType = _markerType;
-	this.coOrdinates = _coOrdinates;
-	return this.init();
+  return this.init(_coOrdinates);
 };
 
-Marker.prototype.init = function() {
-  this.markerIcon = this.getIcon();
-  return L.marker(this.coOrdinates, {icon: this.markerIcon});
+Marker.prototype.init = function(_coOrdinates) {
+  this.mapMarkerIcon = this.getIcon();
+  this.mapMarker = L.marker(_coOrdinates, {icon: this.mapMarkerIcon});
+  return this;
+};
+
+Marker.prototype.addToMap = function(_map) {
+  this.mapMarker.addTo(_map);
+};
+
+Marker.prototype.setLatLng = function(lat, lng) {
+  var newLatLng = new L.LatLng(lat, lng);
+  this.mapMarker.setLatLng(newLatLng);
 };
 
 Marker.prototype.getIcon = function() {
@@ -20,20 +29,31 @@ Marker.prototype.getIcon = function() {
 			 icon = L.icon({
 				iconUrl: '../images/icons/bike-station-icon.png',
 				iconRetinaUrl: '../images/icons/bike-station-icon@2x.png',
-				iconSize: [40, 62],
-				iconAnchor: [20, 61],
+				iconSize: [20, 31],
+				iconAnchor: [10, 30],
 				popupAnchor: [-3, -76],
-				shadowSize: [68, 95],
-				shadowAnchor: [22, 94]
+				shadowSize: [0, 0],
+				shadowAnchor: [0, 0]
+			});
+			break;
+    case 'station-static':
+			 icon = L.icon({
+				iconUrl: '../images/icons/bike-station-icon-static.png',
+				iconRetinaUrl: '../images/icons/bike-station-icon-static@2x.png',
+				iconSize: [20, 31],
+				iconAnchor: [10, 30],
+				popupAnchor: [-3, -76],
+				shadowSize: [0, 0],
+				shadowAnchor: [0, 0]
 			});
 			break;
 		case 'user':
 			icon = L.icon({
 				iconUrl: '../images/icons/user-icon.png',
 				iconRetinaUrl: '../images/icons/user-icon@2x.png',
-				iconSize: [40, 40],
-				iconAnchor: [20, 20],
-				popupAnchor: [20, 20]
+				iconSize: [20, 20],
+				iconAnchor: [10, 10],
+				popupAnchor: [10, 10]
 			});
 			break;
 		default:
