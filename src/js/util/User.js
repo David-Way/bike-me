c6f81a9f8265f2beb38b667b55a3c84b64ad3b03
David Way <dway@ie.ibm.com>
2017-05-13 15:58:22 +0100

    Renamed user object

--- src/js/util/User.js
+++ src/js/util/User.js
@@ -3,20 +3,20 @@
 var Marker = require('./Marker');
 var Promise = require('bluebird');
 
-var UserLocation = function(_mapConfig) {
+var User = function(_mapConfig) {
   this.mapConfig = _mapConfig;
   return this.init();
 };
 
-UserLocation.prototype.init = function() {
+User.prototype.init = function() {
   if ('geolocation' in navigator) {
-    this.userLocationMarker = new Marker('user', [53.3470, -6.2589]);
+    this.UserMarker = new Marker('user', {lat: 53.3470, lng: -6.2589});
     new Promise(function(resolve) {
       this.geolocationWatchID = navigator.geolocation.watchPosition(resolve);
     }.bind(this)).bind(this).then(function(userPosition) {
-      this.userGeolocationSuccess(userPosition);
+      this.geolocationSuccess(userPosition);
     }).catch(function(error) {
-      this.userGeolocationError(error);
+      this.geolocationError(error);
     });
   } else {
     console.log('Geolocation is not available.');
@@ -25,20 +25,19 @@ UserLocation.prototype.init = function() {
   return this;
 };
 
-UserLocation.prototype.addToMap = function(_map) {
-  console.log(this.userLocationMarker);
-  this.userLocationMarker.addToMap(_map);
+User.prototype.addToMap = function(_map) {
+  this.UserMarker.addToMap(_map);
 };
 
-UserLocation.prototype.userGeolocationSuccess = function(userPosition) {
+User.prototype.geolocationSuccess = function(userPosition) {
   var lat = userPosition.coords.latitude;
   var lng = userPosition.coords.longitude;
-  this.userLocationMarker.setLatLng(lat, lng);
+  this.UserMarker.setLatLng({'lat': lat, 'lng': lng});
 };
 
-UserLocation.prototype.userGeolocationError = function(error) {
+User.prototype.geolocationError = function(error) {
   console.log('userGeolocationError', error);
   navigator.geolocation.clearWatch(this.geolocationWatchID);
 };
 
-module.exports = UserLocation;
+module.exports = User;
