cecaddebdf68a55894aadd9fce473b6a9f59aa5e
David Way <dway@ie.ibm.com>
2017-05-13 16:04:21 +0100

    Added station list object to manage stations

---
+++ src/js/models/StationList.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var Station = require('./Station');
+
+var StationList = function(_apiEndpoint, _map) {
+  this.map = _map;
+  this.staticStationsDataURL = '/data/Dublin.json';
+  this.stationsAPIEndpoint = _apiEndpoint + '/stations';
+  this.stations = [];
+  return this.init();
+};
+
+StationList.prototype.init = function(apiEndpoint) {
+  this.loadStaticStationData();
+  this.loadLatestDynamicStationData();
+  return this;
+};
+
+StationList.prototype.stationListed = function (stationData) {
+  for (var i = 0; i < this.stations.length; i++) {
+    if (this.stations[i].number === stationData.number) {
+      return new Station(stationData);
+    }
+  }
+  return false;
+};
+
+StationList.prototype.updateStations = function (stationsData) {
+  for (var i = 0; i < stationsData.length; i++) {
+    var stationData = stationsData[i];
+    var listedStation = this.stationListed(stationData);
+    if (listedStation) { //update it
+      this.stations[i].removeFromMap(this.map);
+      this.stations[i] = listedStation;
+      this.stations[i].addToMap(this.map);
+    } else { // create it
+      var station = new Station(stationData);
+      station.addToMap(this.map);
+      this.stations.push(station);
+    }
+  }
+};
+
+StationList.prototype.loadStaticStationData = function () {
+  var request = new XMLHttpRequest();
+  request.open('GET', this.staticStationsDataURL, true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      this.updateStations(data);
+    } else { // TODO handle Error
+
+    }
+  }.bind(this);
+  request.onerror = function(error) { // TODO handle Error
+    console.log('Error loading latest dynamic station data:', error);
+  };
+  request.send();
+};
+
+StationList.prototype.loadLatestDynamicStationData = function() {
+  var request = new XMLHttpRequest();
+  request.open('GET', this.stationsAPIEndpoint, true);
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      this.updateStations(data);
+    } else { // TODO handle Error
+
+    }
+  }.bind(this);
+  request.onerror = function(error) { // TODO handle Error
+    console.log('Error loading latest dynamic station data:', error.target.status);
+  };
+
+  request.send();
+};
+
+module.exports = StationList;
