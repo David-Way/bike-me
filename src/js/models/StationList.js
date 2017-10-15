38b49ac077ac9c76a48249406de18794a34a9e89
David Way <dway@ie.ibm.com>
2017-10-15 13:22:23 +0100

    :new: Station list now uses loading bar and toggles selection state of stations

--- src/js/models/StationList.js
+++ src/js/models/StationList.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var Station = require('./Station');
 var CONFIG = require('../config');
+var Station = require('./Station');
+var LoadingBar = require('../component/LoadingBar');
 
 var StationList = function(_User, _apiEndpoint, _map, _routeController) {
   this.User = _User;
@@ -10,8 +11,10 @@ var StationList = function(_User, _apiEndpoint, _map, _routeController) {
   this.staticStationsDataURL = '/data/Dublin.json';
   this.stationsAPIEndpoint = _apiEndpoint + '/stations';
   this.stations = [];
+  this.selectedStation = null;
   this.loadLatestDynamicStationDataWatchId = null;
   this.infoCardTemplate = document.getElementById('infoCardTemplate').innerHTML;
+
   return this.init();
 };
 
@@ -29,7 +32,13 @@ StationList.prototype.init = function() {
 StationList.prototype.stationListed = function (stationData) {
   for (var i = 0; i < this.stations.length; i++) {
     if (this.stations[i].number === stationData.number) {
-      return new Station(this.User, stationData, this.infoCardTemplate, this.routeController);
+      return new Station(
+        this.User,
+        stationData,
+        this.infoCardTemplate,
+        this.routeController,
+        this.stationSelectedCallback.bind(this)
+      );
     }
   }
   return false;
@@ -39,16 +48,20 @@ StationList.prototype.updateStations = function (stationsData) {
   for (var i = 0; i < stationsData.length; i++) {
     var stationData = stationsData[i];
     var listedStation = this.stationListed(stationData);
-    if (listedStation) { // update it
+    if (listedStation) { // update it if its listed
       this.stations[i].removeFromMap(this.map);
       this.stations[i] = listedStation;
       this.stations[i].addToMap(this.map);
-    } else { // create it
+      if (this.stations[i].getSelectedState()) { // if the station is selected
+        this.stations[i].showInfoPanel();        // get it to 'update' its info panel
+      }
+    } else { // create it if it's not listed
       var station = new Station(
         this.User,
         stationData,
         this.infoCardTemplate,
-        this.routeController
+        this.routeController,
+        this.stationSelectedCallback.bind(this)
       );
       station.addToMap(this.map);
       this.stations.push(station);
@@ -56,7 +69,29 @@ StationList.prototype.updateStations = function (stationsData) {
   }
 };
 
+StationList.prototype.stationSelectedCallback = function (_selectedStation) {
+  this.selectedStation = _selectedStation;
+  for (var i = 0; i < this.stations.length; i++) {
+    if (this.stations[i].number !== _selectedStation.number) {
+      this.stations[i].setSelectedState(false);
+    } else { // found selected station
+      this.stations[i].toggleSelectedState();
+      if (this.stations[i].getSelectedState()) {
+        this.stations[i].showInfoPanel();
+        this.stations[i].showDirections();
+      } else { //hide info panel
+        this.stations[i].hideInfoPanel();
+        this.stations[i].hideDirections();
+      }
+    }
+  }
+};
+
+// TODO DRY this out - loadStaticStationData ~== loadLatestDynamicStationData
 StationList.prototype.loadStaticStationData = function () {
+  var ctx = document.getElementsByClassName('leaflet-top leaflet-center')[0];
+  var loadingBar = new LoadingBar(ctx);
+  loadingBar.init();
   var request = new XMLHttpRequest();
   request.open('GET', this.staticStationsDataURL, true);
   request.onload = function() {
@@ -64,16 +99,21 @@ StationList.prototype.loadStaticStationData = function () {
       var data = JSON.parse(request.responseText);
       this.updateStations(data);
     } else { // TODO handle Error
-
+      loadingBar.remove();
     }
+    loadingBar.finish();
   }.bind(this);
   request.onerror = function(error) { // TODO handle Error
-    console.log('Error loading latest dynamic station data:', error);
+    console.log('Error loading latest static station data:', error);
+    loadingBar.remove();
   };
-  request.send();
+  return request.send();
 };
 
 StationList.prototype.loadLatestDynamicStationData = function() {
+  var ctx = document.getElementsByClassName('leaflet-top leaflet-center')[0];
+  var loadingBar = new LoadingBar(ctx);
+  loadingBar.init();
   var request = new XMLHttpRequest();
   request.open('GET', this.stationsAPIEndpoint, true);
   request.onload = function() {
@@ -82,26 +122,15 @@ StationList.prototype.loadLatestDynamicStationData = function() {
       this.updateStations(data);
     } else { // TODO handle Error
       clearTimeout(this.loadLatestDynamicStationDataWatchId);
+      loadingBar.remove();
     }
-    this.hideLoadingIcon();
+    loadingBar.finish();
   }.bind(this);
   request.onerror = function(error) { // TODO handle Error
     console.log('Error loading latest dynamic station data:', error.target.status);
-    this.hideLoadingIcon();
+    loadingBar.remove();
   };
-
-  this.showLoadingIcon();
-  request.send();
-};
-
-StationList.prototype.showLoadingIcon = function () {
-  //test
-  console.log('showing loading icon');
-};
-
-StationList.prototype.hideLoadingIcon = function () {
-  //test 2
-  console.log('hiding loading icon');
+  return request.send();
 };
 
 module.exports = StationList;
